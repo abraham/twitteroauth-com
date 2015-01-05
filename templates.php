2bc6f81571583b4c5d7a8d49e3d1178a881d0ce2
Abraham Williams <4braham@gmail.com>
2015-01-04 22:13:48 -0800

    enable template cache by flag

--- templates.php
+++ templates.php
@@ -3,6 +3,8 @@
 $loader = new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . 'templates');
 $twig = new Twig_Environment($loader, array(
     'debug' => true,
-    // 'cache' => __DIR__ . DIRECTORY_SEPARATOR . 'cache',
 ));
+if (getenv('TEMPLATE_CACHE_ENABLED') && getenv('TEMPLATE_CACHE_ENABLED') != 'false') {
+    $twig->setCache(__DIR__ . DIRECTORY_SEPARATOR . 'cache');
+}
 $twig->addExtension(new Twig_Extension_Debug());
