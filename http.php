eee2ed6e75c9e1e67f84628f52f2e15260521265
Abraham Williams <4braham@gmail.com>
2015-01-19 12:49:13 -0800

    remove debug

--- http.php
+++ http.php
@@ -4,10 +4,6 @@ define('FORCE_TLS', getenv('FORCE_TLS') === 'true');
 $httpHost = $_SERVER['HTTP_HOST'];
 $expectedHost = getenv('HOST');
 
-foreach ($_SERVER as $name => $value) {
-    error_log("HEADER: $name: $value");
-}
-
 if (!DEBUG) {
     if (
         FORCE_TLS &&
