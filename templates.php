5a6f455cd7673443d0d9c43b711951e08c9d0592
Abraham Williams <4braham@gmail.com>
2016-01-15 13:29:19 -0600

    Revert template change so that the live snippets work

--- templates.php
+++ templates.php
@@ -1,7 +1,7 @@
 <?php
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . 'templates');
-$twig = new Twig_Environment($loader);
+$twig = new Twig_Environment($loader, ['debug' => true]);
 if (getenv('TEMPLATE_CACHE_ENABLED') && getenv('TEMPLATE_CACHE_ENABLED') === 'true') {
     $twig->setCache(__DIR__ . DIRECTORY_SEPARATOR . 'cache');
 }
